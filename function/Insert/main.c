7048c26e118fdc52eb1c7fb97a2b2b89529b3732
thangved <thangcreation@gmail.com>
2021-08-11 16:15:23 +0700

    : D

--- function/Insert/main.c
+++ function/Insert/main.c
@@ -1,9 +1,26 @@
-void Insert(int x, int p, int A[], int *pn)
+#include <stdlib.h>
+#include <stdio.h>
+
+void Insert(int x, int p, int A[100], int *pn)
 {
-    for (int i = *pn; i >= p && i > 0; i--)
+    for (int i = *pn; i > p; i--)
         A[i] = A[i - 1];
 
     A[p] = x;
-
     *pn = *pn + 1;
+}
+
+int main(int argc, char const *argv[])
+{
+    int n = 7;
+    int A[] = {1, 2, 3, 4, 5, 6, 7};
+
+    Insert(0, 0, A, &n);
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", A[i]);
+    }
+
+    return 0;
 }
