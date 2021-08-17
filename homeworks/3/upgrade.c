ec3569a05c5e35b6d69af244b1b97c15a8982f52
thangved <thangcreation@gmail.com>
2021-08-17 13:10:04 +0700

    : D

--- homeworks/3/upgrade.c
+++ homeworks/3/upgrade.c
@@ -4,13 +4,13 @@ double exp(double x, int n)
         a = 1;    //a_0
     for (int i = 1; i <= n; i++)
     {
-        a *= x / i; //a_n = a_(n-1) * x/n
+        a *= x / i; //a_i = a_(i-1) * x/i
         e += a;
     }
     return e;
 }
 int main(int argc, char const *argv[])
 {
-    double a = exp(1.2, 2);
+    double a = exp(1.2, 200);
     return 0;
 }
